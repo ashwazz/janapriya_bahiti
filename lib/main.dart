2f70c5cc81e2442403f041af5f294290a1f32e5a
ashwazz <127042776+ashwazz@users.noreply.github.com>
2024-10-01 02:52:18 +0530

    Changes to be committed: 	modified:   lib/main.dart
    also made scroll animation smoother and made it work
    just have to make it fit all screen sizes

--- lib/main.dart
+++ lib/main.dart
@@ -32,9 +32,9 @@ class _WebAppState extends State<WebApp> with SingleTickerProviderStateMixin{
   void _scrollToItem(int index) {
     if (_scrollController.hasClients) {
       _scrollController.animateTo(
-        index * MediaQuery.of(context).size.height,
+        index * MediaQuery.of(context).size.height*1.1,
         duration: const Duration(seconds: 2),
-        curve: Curves.ease,
+        curve: Curves.easeOutSine,
       );
       // print("has clients");
     } else {
@@ -44,10 +44,14 @@ class _WebAppState extends State<WebApp> with SingleTickerProviderStateMixin{
   @override
   void initState() {
     
-    _animationController = AnimationController(vsync: this, duration: const Duration(seconds: 5));
+    _animationController = AnimationController(vsync: this, duration: const Duration(seconds: 3),
+    upperBound: 1,
+    lowerBound: 0
+    
+    );
      _scrollController.addListener((){
       print(_scrollController.offset/1000);
-      _animationController.value = _scrollController.offset/900;
+      _animationController.value = (_scrollController.offset/1000)>1?1:(_scrollController.offset/1000);
      });
      super.initState();
   } 
@@ -67,7 +71,7 @@ class _WebAppState extends State<WebApp> with SingleTickerProviderStateMixin{
             controller: _scrollController,
             scrollDirection: Axis.vertical,
             children: [
-              slide1(onPressed: () => _scrollToItem(2)),
+              slide1(onPressed: () => _scrollToItem(1)),
               slide2(),
               slide3(),
             ],
@@ -77,16 +81,16 @@ class _WebAppState extends State<WebApp> with SingleTickerProviderStateMixin{
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Row(
-                mainAxisAlignment: MainAxisAlignment.end,
+                mainAxisAlignment: MainAxisAlignment.start,
                
                 children: [
                   Container(
-                    margin: EdgeInsets.only(right: MediaQuery.of(context).size.width*0.05),
-                    width: MediaQuery.of(context).size.width*0.3,
-                    height: MediaQuery.of(context).size.height*0.8,
-                    color: Color.fromARGB(255, 0, 255, 60),
-                    child: Lottie.asset('assets/lottie/raytraced22_lottie.json' , controller: _animationController
-                    , 
+                    //margin: EdgeInsets.only(right: MediaQuery.of(context).size.width*0.05),
+                   // width: MediaQuery.of(context).size.width*0.3,
+                    height: MediaQuery.of(context).size.height*0.94,
+                    color: Color.fromARGB(5, 0, 255, 60),
+                    child: Lottie.asset('assets/lottie/1st_seq_lottie.json' , controller: _animationController
+                    , fit: BoxFit.fill,
                     // onLoaded: (animation) {
                   
                     //   _animationController
@@ -115,7 +119,7 @@ class slide1 extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       width: MediaQuery.of(context).size.width,
-      height: MediaQuery.of(context).size.height,
+      height: MediaQuery.of(context).size.height*1,
       color: Colors.red,
       child: Center(
         child: ElevatedButton(
